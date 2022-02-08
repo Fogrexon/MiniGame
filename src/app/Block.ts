b7798ecd76c6e5f4fc86956ecf41220ef37aee1e
FogRexOn <fogrexon@gmail.com>
2022-02-09 02:54:57 +0900

    add ai

--- src/app/Block.ts
+++ src/app/Block.ts
@@ -1,14 +1,11 @@
 import { Mesh, BoxGeometry, MeshBasicMaterial, Color } from "three";
 import { IRenderable } from "./IRenderable";
 
+type Road = 'none' | 'vertical' | 'horizontal' | 'both' | 'building';
 export class Block extends Mesh implements IRenderable {
-  private elevated = false;
+  private road: Road = 'none';
 
-  private light = 0;
-
-  private postlight = 0;
-
-  private lightHistory = [0, 0];
+  private postroad: Road = 'none';
 
   // eslint-disable-next-line no-use-before-define
   private blockGetter: (x: number, y: number) => Block;
@@ -27,36 +24,103 @@ export class Block extends Mesh implements IRenderable {
   }
 
   public onClick() {
-    this.light = 5;
+    this.postroad = Math.random() > 0.5 ? 'vertical' : 'horizontal';
   }
 
-  public getLight() {
-    return this.light;
+  public getRoad() {
+    return this.road;
   }
 
   public prerender() {
 
-    let sum = 0;
+    if(this.road === 'both' || this.road === 'building') return
+    if(this.road === 'vertical') {
+      for(let dy = -1; dy < 2; dy += 2) {
+        const block = this.blockGetter(this.x, this.y + dy);
+        if(block && block.getRoad() === 'building') {
+          return;
+        }
+      }
+      this.postroad = Math.random() < 0.998 ? this.road : 'both';
+      return; 
+    }
+    if(this.road === 'horizontal') {
+      for(let dx = -1; dx < 2; dx += 2) {
+        const block = this.blockGetter(this.x + dx, this.y);
+        if(block && block.getRoad() === 'building') {
+          return;
+        }
+      }
+      this.postroad = Math.random() < 0.998 ? this.road : 'both';
+      return; 
+    }
+
     for(let dx = -1; dx < 2; dx += 2) {
       const block = this.blockGetter(this.x + dx, this.y);
-      if(block) sum += block.getLight();
-      else sum += -this.light;
+      if(block) {
+        const road = block.getRoad();
+        if((road === 'horizontal' || road === 'both') && Math.random() < 0.03) {
+          // this.postroad = Math.random() < 0.95 ? 'horizontal' : 'both';
+          this.postroad = 'horizontal';
+        } else if(road === 'vertical' && Math.random() < 0.02) {
+          this.postroad = 'building';
+        }
+      }
     }
     for(let dy = -1; dy < 2; dy += 2) {
       const block = this.blockGetter(this.x, this.y + dy);
-      if(block) sum += block.getLight();
-      else sum += -this.light;
+      if(block) {
+        const road = block.getRoad();
+        if((road === 'vertical' || road === 'both') && Math.random() < 0.03) {
+          // this.postroad = Math.random() < 0.95 ? 'vertical' : 'both';
+          this.postroad = 'vertical';
+        } else if(road === 'horizontal' && Math.random() < 0.02) {
+          this.postroad = 'building';
+        }
+      }
     }
-    this.postlight = 2 * this.light - this.lightHistory[0] + (4 / 60 / 60 / 0.1 / 0.1) * (sum - this.light * 4);
-    // this.postlight *= 0.99;
-
   }
   
   public render() {
-    this.lightHistory = [this.light, this.lightHistory[0]];
-    this.light = this.postlight;
-    this.position.y = this.light;
-    const value = this.light ** 32;
+    this.road = this.postroad;
+    let value = 0;
+    switch(this.road) {
+      case 'vertical': {
+        this.scale.x = 0.5;
+        this.scale.y = 0.5;
+        this.scale.z = 1;
+        value = 1.0;
+        break;
+      }
+      case 'horizontal': {
+        this.scale.x = 1;
+        this.scale.y = 0.5;
+        this.scale.z = 0.5;
+        value = 1.0;
+        break;
+      }
+      case 'both': {
+        this.scale.x = 1;
+        this.scale.y = 0.5;
+        this.scale.z = 1;
+        value = 1.0;
+        break;
+      }
+      case 'building': {
+        this.scale.x = 1;
+        this.scale.y = 2;
+        this.scale.z = 1;
+        value = 0.5;
+        break;
+      }
+      default: {
+        this.scale.x = 0.5;
+        this.scale.y = 0.5;
+        this.scale.z = 0.5;
+        value = 0.0;
+        break;
+      }
+    }
     (this.material as MeshBasicMaterial).color = new Color(value, value, value);
   }
 }
